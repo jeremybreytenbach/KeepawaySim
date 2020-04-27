28b58823b0334ea8b4050d5c2ba76c20f3129580
Jeremy Breytenbach <jeremybreytenbach@gmail.com>
2020-04-27 17:07:42 +0200

    Changed handling of position

--- HyperNEAT Keepaway/Sources/KeepAway/MAP-Elites/MapElites.cs
+++ HyperNEAT Keepaway/Sources/KeepAway/MAP-Elites/MapElites.cs
@@ -34,46 +34,12 @@ namespace Keepaway
                 double no_passes = genomes[i].BehaviorType.bVector[2];
                 double distfromcentre = genomes[i].BehaviorType.bVector[3];
 
-                // round each dimension
-                teamDispersion = Math.Round(teamDispersion * EliteMap.dimensionMax);
-                no_passes = Math.Round(no_passes * EliteMap.dimensionMax);
-                distfromcentre = Math.Round(distfromcentre * EliteMap.dimensionMax);
-
-                // update EliteMap max to new max if necessary
-                //if (teamDispersion > EliteMap.dimensionsMax[0])
-                //{
-                //    EliteMap.dimensionsMax[0] = (int)teamDispersion;
-                //}
-                //if (no_passes > EliteMap.dimensionsMax[1])
-                //{
-                //    EliteMap.dimensionsMax[1] = (int)no_passes;
-                //}
-                //if (distfromcentre > EliteMap.dimensionsMax[2])
-                //{
-                //    EliteMap.dimensionsMax[2] = (int)distfromcentre;
-                //}
-
-                // update EliteMap min to new min if necessary
-                //if (teamDispersion < EliteMap.dimensionsMin[0])
-                //{
-                //    EliteMap.dimensionsMin[0] = (int)teamDispersion;
-                //}
-                //if (no_passes < EliteMap.dimensionsMin[1])
-                //{
-                //    EliteMap.dimensionsMin[1] = (int)no_passes;
-                //}
-                //if (distfromcentre < EliteMap.dimensionsMin[2])
-                //{
-                //    EliteMap.dimensionsMin[2] = (int)distfromcentre;
-                //}
-
                 int[] position = new int[3]; // x,y,z position, eg 0,0,0 or 0,3,1 etc.
-                //position[0] = (EliteMap.dimensionsMax[0] - EliteMap.dimensionsMin[0]) / eliteMap.elementResolution[0] - 1;
-                //position[1] = (EliteMap.dimensionsMax[1] - EliteMap.dimensionsMin[1]) / eliteMap.elementResolution[1] - 1;
-                //position[2] = (EliteMap.dimensionsMax[2] - EliteMap.dimensionsMin[2]) / eliteMap.elementResolution[2] - 1;
-                position[0] = (int)teamDispersion - 1;
-                position[1] = (int)no_passes - 1;
-                position[2] = (int)distfromcentre - 1;
+                // round each dimension and normalise to fit in map (divide by FitNormaliser, which should usually by = 10)
+                // subtract 1 to ???
+                position[0] = (int)Math.Round(teamDispersion / Program.config.FitNormaliser) - 1;
+                position[1] = (int)Math.Round(no_passes / Program.config.FitNormaliser) - 1;
+                position[2] = (int)Math.Round(distfromcentre / Program.config.FitNormaliser) - 1;
 
                 if (position[0] < 1)
                 {
